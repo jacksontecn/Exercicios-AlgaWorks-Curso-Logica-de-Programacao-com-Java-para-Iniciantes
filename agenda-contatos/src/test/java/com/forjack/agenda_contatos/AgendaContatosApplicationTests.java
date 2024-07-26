6201bbb970b5726e29926c549478761cda8f74ff
jacksontecn <arakisu@gmail.com>
2024-07-26 11:03:10 -0300

    Commitando projeto criado para aplicação web com springboot para uma agenda de contatos.

---
+++ agenda-contatos/src/test/java/com/forjack/agenda_contatos/AgendaContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forjack.agenda_contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
